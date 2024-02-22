b0e166dec8d3b984a413ab1d6aa2dc7b738c44e5
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-22 11:27:57 -0500

    reducers added to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,41 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
-// Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+
+const pizzas = (state = [], action) => {
+  if(action.type === 'REFRESH_PIZZAS') {
+    return action.payload;
+  }
+  return state;
+}
+
+const cart = (state = [], action) => {
+  if(action.type === 'ADD_TO_CART') {
+    console.log(`pizza added to cart: ${action.payload}`);
+    return [...state, action.payload];
+  }
+  if(action.type === 'CLEAR_CART') {
+    return state = [];
+  }
+  return state;
+}
+
+const customerInfo = (state = [], action) => {
+  if(action.type === 'ADD_CUSTOMER') {
+    console.log(`customer info added: ${action.payload}`);
+    return [...state, action.payload];
+  }
+  if(action.type === 'CLEAR_CUSTOMER') {
+    return state = [];
+  }
   return state;
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    pizzas,
+    cart,
+    customerInfo 
   }),
   applyMiddleware(logger),
 );
